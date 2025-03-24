5319cd8d49a38a34f1224b9b4ffa557d43d16f22
eshika nahata <eshikanahata@gmail.com>
2025-03-24 15:14:19 +0530

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from ai4bharat.transliteration import XlitEngine
-e = XlitEngine(src_script_type="indic", beam_width=10, rescore=False)
-out = e.translit_word("नमस्ते", lang_code="hi", topk=5)
-print(out)
-# output: ['namaste', 'namastey', 'namasthe', 'namastay', 'namste']
